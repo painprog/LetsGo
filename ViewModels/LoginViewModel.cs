0971cc61ccf5bbba9c55952ad75fb098fa77dd75
Vladimir Bedash <vvbedash@gmail.com>
2021-11-04 14:25:05 +0600

    #1 Создана модель представления страницы авторизации, а так же сама страница.

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login или почта")]
+        public string LoginOrEmail { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+    }
+}

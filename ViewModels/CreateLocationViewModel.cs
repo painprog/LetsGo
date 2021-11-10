c8984ebde653c16b8b527b11528af197077e6830
Irina Lobacheva <ie.lobacheva@gmail.com>
2021-11-11 03:31:49 +0600

    #6 Создана модель представления и предствление для создания площадки.

---
+++ ViewModels/CreateLocationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LetsGo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace LetsGo.ViewModels
+{
+    public class CreateLocationViewModel
+    {
+        public Location Location { get; set; }
+        public IList<SelectListItem> LocationCategories { get; set; }
+        public string PhoneNums { get; set; }
+    }
+}

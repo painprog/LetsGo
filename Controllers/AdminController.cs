05b76fc82d227f8a3d357c23f3cb9a1128dcd238
Vladimir Bedash <vvbedash@gmail.com>
2021-11-16 18:28:56 +0600

    #13 Создан контроллер AdminController. С методом прогрузки личного кабинета, на котором будет инфа об админе и список мероприятий.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using LetsGo.Models;
+using LetsGo.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.Controllers
+{
+    [Authorize(Roles ="superadmin, admin")]
+    public class AdminController : Controller
+    {
+        private readonly LetsGoContext _goContext;
+
+        public AdminController(LetsGoContext goContext)
+        {
+            _goContext = goContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile(string adminId)
+        {
+            var admin = await _goContext.ContextUsers.FirstOrDefaultAsync(a => a.Id == adminId);
+            if(admin != null)
+            {
+                ViewBag.Events = await _goContext.Events.OrderByDescending(e => e.CreatedAt).ToListAsync();
+                return View(admin);
+            }
+            return BadRequest();
+        }
+    }
+}

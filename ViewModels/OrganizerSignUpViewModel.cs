f33aab1cab110f9817062615e8522e65e6415d31
Irina Lobacheva <ie.lobacheva@gmail.com>
2021-11-17 15:26:28 +0600

    #11 Создана OrganizerSignUpViewModel и представление OrganizerSignUp для регистрации организатора.

---
+++ ViewModels/OrganizerSignUpViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace LetsGo.ViewModels
+{
+    public class OrganizerSignUpViewModel
+    {
+        [Required(ErrorMessage = "Данное поле обязательно для заполнения")]
+        public string Username { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Данное поле обязательно для заполнения")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Данное поле обязательно для заполнения")]
+        public string PhoneNumber { get; set; }
+
+        public IFormFile Avatar { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Введите пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Повторите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}

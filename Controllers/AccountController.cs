7d4dafffabb682690ad3217e8c3914f6013c1e2a
Irina Lobacheva <ie.lobacheva@gmail.com>
2021-11-17 15:24:09 +0600

    #11 Создан метод OrganizerSignUp в AccountController для регистрации организатора.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,9 @@
 ﻿using LetsGo.Models;
+using LetsGo.Services;
 using LetsGo.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LetsGo.Controllers
@@ -21,14 +20,17 @@ namespace LetsGo.Controllers
             _signInManager = signInManager;
             _context = context;
         }
+
+
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 User user = await _userManager.FindByEmailAsync(model.LoginOrEmail) ?? await _userManager.FindByNameAsync(model.LoginOrEmail);
                 Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(
@@ -42,6 +44,50 @@ namespace LetsGo.Controllers
                     return RedirectToAction("Index", "Home");
                 }
                 ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            }
+            return View(model);
+        }
+
+        public IActionResult OrganizerSignUp() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> OrganizerSignUp(OrganizerSignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userEmailCheck = await _userManager.FindByEmailAsync(model.Email);
+                if (userEmailCheck == null)
+                {
+                    string name = EventsService.GenerateCode() + '.' + Path.GetExtension(model.Avatar.FileName);
+                    string pathImage = "/avatars/" + name;
+                    using (var fileStream = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\" + pathImage), FileMode.Create))
+                        await model.Avatar.CopyToAsync(fileStream);
+
+                    User user = new User
+                    {
+                        UserName = model.Username,
+                        Email = model.Email,
+                        AvatarLink = pathImage,
+                        PhoneNumber = model.PhoneNumber,
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        await _userManager.AddToRoleAsync(user, "organizer");
+                        await _signInManager.SignInAsync(user, false);
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, $"Пользователь с таким электронным адресом уже зарегистрирован.");
+                    return View(model);
+                }
             }
             return View(model);
         }

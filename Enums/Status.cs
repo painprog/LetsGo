d19f3c8b0c04f026c7b001b80a0a05bf4a931ef5
Irina Lobacheva <ie.lobacheva@gmail.com>
2021-11-17 11:32:13 +0600

    #11 Создала Status enum и изменила тип поля Status на enum, создала миграцию.

---
+++ Enums/Status.cs
@@ -0,0 +1,13 @@
+﻿namespace LetsGo.Enums
+{
+    public enum Status
+    {
+        NotDefined = 0,
+        New = 1,
+        Rejected = 2,
+        Published = 3,
+        UnPublished = 4,
+        Edited = 5,
+        Expired = 6
+    }
+}
--- Services/EventsService.cs
+++ Services/EventsService.cs
@@ -1,4 +1,5 @@
-﻿using LetsGo.Models;
+﻿using LetsGo.Enums;
+using LetsGo.Models;
 using LetsGo.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -48,7 +49,7 @@ namespace LetsGo.Services
                 Categories = jsonCateg,
                 AgeLimit = Convert.ToInt32(eventView.AgeLimit),
                 TicketLimit = eventView.TicketLimit,
-                Status = "checking",
+                Status = Status.New,
                 LocationId = _goContext.Locations.FirstOrDefault(l => l.Name == eventView.Location).Id,
                 OrganizerId = eventView.OrganizerId
             };

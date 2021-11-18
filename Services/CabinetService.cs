9927340f392c582960b3bae5d79ab3939a044448
Vladimir Bedash <vvbedash@gmail.com>
2021-11-18 18:38:31 +0600

    #13 Создан сервис личного кабинета.

---
+++ Services/CabinetService.cs
@@ -0,0 +1,23 @@
+﻿using LetsGo.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.Services
+{
+    public class CabinetService
+    {
+        private readonly EventsService _service;
+        private readonly LetsGoContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public CabinetService(EventsService service, LetsGoContext context, UserManager<User> userManager)
+        {
+            _service = service;
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}

5fe9a84d75a09b1377988642b1e3cb55ca5bb852
Vladimir Bedash <vvbedash@gmail.com>
2021-11-19 10:56:38 +0600

    #13 Для удобства слегка изменена вёрстка. Добавлено модальное окно для указания причины отмены (и других действий) мероприятия. Добавлены скрипты для отправки запроса на изменение статуса мероприятия.

---
+++ ViewModels/EventEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using LetsGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.ViewModels
+{
+    public class EventEditViewModel
+    {
+        public Event Event { get; set; }
+        public List<Location> Locations { get; set; }
+        public List<EventTicketType> EventTicketTypes { get; set; }
+
+    }
+}

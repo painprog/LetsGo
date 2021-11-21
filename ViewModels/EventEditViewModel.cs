c0a3c57f7834ca794791426e4432d89941633ab0
Vladimir Bedash <vvbedash@gmail.com>
2021-11-21 11:48:40 +0600

    #13 Создана модель представления для страницы редактирования мероприятия.

---
+++ ViewModels/EventEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LetsGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.ViewModels
+{
+    public class EventEditViewModel
+    {
+        public Event Event { get; set; }
+        public List<EventTicketType> EventTicketTypes { get; set; }
+        public string Tickets { get; set; }
+    }
+}

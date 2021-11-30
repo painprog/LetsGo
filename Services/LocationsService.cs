050f349b67dda1398e9999fb3710b2aedc9dc6ca
Abdilamitov Ikhmat <pro.ihmat@mail.ru>
2021-11-30 18:02:21 +0600

    #28 чутка изменил добавление локации в LocationsService

--- Services/LocationsService.cs
+++ Services/LocationsService.cs
@@ -3,6 +3,7 @@ using LetsGo.ViewModels;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LetsGo.Services
 {
@@ -14,7 +15,7 @@ namespace LetsGo.Services
             _db = db;
         }
 
-        public async void Create(CreateLocationViewModel model)
+        public async Task<Location> Create(CreateLocationViewModel model)
         {
             Location location = new Location()
             {
@@ -22,33 +23,31 @@ namespace LetsGo.Services
                 Address = model.Address,
                 Description = model.Description
             };
-            if (model.LocationCategories.Where(x => x.Selected == true).Count() == 0)
+            var categories = model.LocationCategories.Where(x => x.Selected).Select(x => new
             {
-                var categories = _db.LocationCategories.Where(c => c.Name == "Другое");
-                var categoriesJson = JsonConvert.SerializeObject(categories);
+                Id = x.Value,
+                Name = x.Text
+            });
+            if (categories.Count() == 0)
+            {
+                var category = _db.LocationCategories.FirstOrDefault(c => c.Name == "Другое");
+                var categoriesJson = JsonConvert.SerializeObject(category);
                 location.Categories = categoriesJson;
             }   
             else
             {
-                var categories = model.LocationCategories.Where(x => x.Selected).Select(x => new
-                {
-                    Id = x.Value,
-                    Name = x.Text
-                });
                 var categoriesJson = JsonConvert.SerializeObject(categories);
                 location.Categories = categoriesJson;
             }
             
             List<string> phones = new List<string>();
-            if (model.PhoneNums != null)
-                phones = model.PhoneNums.Split(',').ToList();
-            if (model.Phones != null)
-                phones.Add(model.Phones);
+            phones = model.PhoneNums.Split(',').ToList();
             var phoneNumbersJson = JsonConvert.SerializeObject(phones);
             location.Phones = phoneNumbersJson;
 
             _db.Locations.Add(location);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
+            return location;
         }
     }
 }

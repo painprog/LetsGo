d5cb0c0d5ff9bead5f44ec07ace7114c540480ec
Karina Andriushchenko <karina.andr@icloud.com>
2021-12-13 19:42:31 +0600

    добавила функционал перехода на детальную страницу площадки с главной страницы

--- ViewModels/LocationDetailsViewModel.cs
+++ ViewModels/LocationDetailsViewModel.cs
@@ -9,7 +9,7 @@ namespace LetsGo.ViewModels
     public class LocationDetailsViewModel
     {
         public Location Location { get; set; }
-        public DateTime MaxDate { get; set; }
+        //public DateTime MaxDate { get; set; }
         public List<Event> FutureEvents { get; set; }
         public List<Event> PastEvents { get; set; }
         public List<LocationCategory> LocationCategories { get; set; }

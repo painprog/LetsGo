8ee69b928b49b297aa75c0e82a487e769de3da60
Anastasia799 <88529142+Anastasia799@users.noreply.github.com>
2022-01-21 14:47:18 +0600

    #52 Добавлена локализация ЛК, также добавлена локализация категорий через контроллер

--- src/LetsGo.UI/ViewModels/ProfileViewModel.cs
+++ src/LetsGo.UI/ViewModels/ProfileViewModel.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using LetsGo.Core.Entities;
 using LetsGo.Core.Entities.Enums;
-using LetsGo.Enums;
+
+//using LetsGo.Enums;
 
 namespace LetsGo.UI.ViewModels
 {
@@ -14,7 +15,7 @@ namespace LetsGo.UI.ViewModels
         public DateTime DateTimeBefore { get; set; }
         public DateTime DateTimeFrom { get; set; }
         public Status Status { get; set; }
-        public SortState SortOrder { get; set; }
+     //   public SortState SortOrder { get; set; }
         public List<EventCategory> EventCategories { get; set; }
         public string EventCategory { get; set; }
         public Dictionary<string, Status> Stats { get; set; }

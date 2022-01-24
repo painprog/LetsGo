15dc2ac5106dbfdca926bbf166ac4f5572fa40fa
Irina Lobacheva <ie.lobacheva@gmail.com>
2022-01-24 14:20:05 +0600

    #56 Создана функция фильтрации карточек организаторов в лк.

--- src/LetsGo.UI/wwwroot/js/site.js
+++ src/LetsGo.UI/wwwroot/js/site.js
@@ -7,6 +7,11 @@ $('#all-events').btnLoadmore({
     classBtn: 'btn btn-danger btn-loadmore'
 });
 
+function filter() {
+    filterEvents();
+    filterUsers();
+}
+
 function filterEvents() {
     var input, filter, cards, cardContainer, h5, i, title, location, eventStatus;
     input = document.getElementById("searchbox-input");
@@ -17,9 +22,31 @@ function filterEvents() {
         title = cards[i].querySelector(".pn-head_title h1");
         location = cards[i].querySelector(".pn-head_place");
         eventStatus = cards[i].querySelector("#event-status");
+
         if (title.innerText.toUpperCase().indexOf(filter) > -1
             || location.innerText.toUpperCase().indexOf(filter) > -1
             || eventStatus.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+function filterUsers() {
+    var input, filter, cards, cardContainer, i, organaizerName, organaizerEmail, organaizerPhone;
+    input = document.getElementById("searchbox-input");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("all-organizers");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        organaizerName = cards[i].querySelector("#organizer-name").innerText;
+        organaizerPhone = cards[i].querySelector("#organizer-phone").innerText;
+        organaizerEmail = cards[i].querySelector("#organizer-email").innerText;
+
+        if (organaizerName.toUpperCase().indexOf(filter) > -1
+            || organaizerEmail.toUpperCase().indexOf(filter) > -1
+            || organaizerPhone.toUpperCase().indexOf(filter) > -1) {
             cards[i].style.display = "";
         } else {
             cards[i].style.display = "none";

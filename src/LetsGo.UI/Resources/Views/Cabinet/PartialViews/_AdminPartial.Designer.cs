693c5ca3f0a8cd543baa6107fce124ffff3bf610
Karina Andriushchenko <karina.andr@icloud.com>
2022-01-26 17:45:07 +0600

    #57 добавлена локализация под добавленные вставки

--- src/LetsGo.UI/Resources/Views/Cabinet/PartialViews/_AdminPartial.Designer.cs
+++ src/LetsGo.UI/Resources/Views/Cabinet/PartialViews/_AdminPartial.Designer.cs
@@ -19,7 +19,7 @@ namespace LetsGo.UI.Resources.Views.Cabinet.PartialViews {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class _AdminPartial {
@@ -147,6 +147,15 @@ namespace LetsGo.UI.Resources.Views.Cabinet.PartialViews {
         internal static string Mobile {
             get {
                 return ResourceManager.GetString("Mobile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string NotConfirmedEmail {
+            get {
+                return ResourceManager.GetString("NotConfirmedEmail", resourceCulture);
             }
         }
         
--- src/LetsGo.UI/Resources/Views/Cabinet/Profile.Designer.cs
+++ src/LetsGo.UI/Resources/Views/Cabinet/Profile.Designer.cs
@@ -19,7 +19,7 @@ namespace LetsGo.UI.Resources.Views.Cabinet {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Profile {
@@ -84,6 +84,15 @@ namespace LetsGo.UI.Resources.Views.Cabinet {
         internal static string AddLocation {
             get {
                 return ResourceManager.GetString("AddLocation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string ApprovedUsers {
+            get {
+                return ResourceManager.GetString("ApprovedUsers", resourceCulture);
             }
         }
         
@@ -174,6 +183,15 @@ namespace LetsGo.UI.Resources.Views.Cabinet {
         internal static string Profiles {
             get {
                 return ResourceManager.GetString("Profiles", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string Requests {
+            get {
+                return ResourceManager.GetString("Requests", resourceCulture);
             }
         }
         
